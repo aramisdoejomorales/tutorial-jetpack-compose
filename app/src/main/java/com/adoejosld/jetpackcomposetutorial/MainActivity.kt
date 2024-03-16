4ca35cc16bc424bb8c4856f2c6a581605f9d610e
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-03-16 16:07:10 -0400

    add .

--- app/src/main/java/com/adoejosld/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/adoejosld/jetpackcomposetutorial/MainActivity.kt
@@ -4,25 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adoejosld.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComponents()
+            JetpackComposeTutorialTheme {
+                MyComponents()
+            }
+
+
         }
     }
 }
 
 @Composable
 fun MyComponents() {
-    Row {
+    Row(modifier = Modifier.padding(all = 12.dp)) {
         MyImage()
         MyTexts()
     }
@@ -30,22 +45,28 @@ fun MyComponents() {
 
 @Composable
 fun MyTexts() {
-    Column {
+    Column(modifier = Modifier.padding(all = 8.dp)) {
         MyText(text = "Hola Jetpack Compose")
+        Spacer(modifier = Modifier.heightIn(6.dp))
         MyText("Soy Aramis")
+        Spacer(modifier = Modifier.heightIn(6.dp))
         MyText(text = "Aprendo a Programar")
     }
 }
-
 
 @Composable
 fun MyImage() {
     Image(
         painterResource(R.drawable.ic_launcher_foreground),
-        contentDescription = "Imagen de Prueba"
+        contentDescription = "Imagen de Prueba",
+        modifier = Modifier
+            .size(85.dp)
+            .clip(CircleShape)
+            .background(
+                Color.Cyan
+            )
     )
 }
-
 
 @Composable
 private fun MyText(text: String) {
